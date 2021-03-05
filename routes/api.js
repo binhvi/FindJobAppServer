4a57091e120a36d07f0138ff00c3b5a74e58925a
binhvi <binhvttph07052@fpt.edu.vn>
2021-03-05 14:00:05 +0700

    API get user list

--- routes/api.js
+++ routes/api.js
@@ -1783,55 +1783,29 @@ router.post('/users/change-password', (req, res) => {
 router.get('/users', (req, res) => {
     let selectAllUsersSql =
         "select " +
-        commonResources.USERS_TABLE_NAME + "." +
-        commonResources.USERS_COLUMN_ID + ", " +
+            commonResources.USERS_TABLE_NAME + "." +
+            commonResources.USERS_COLUMN_ID + ", " +
 
-        commonResources.USERS_COLUMN_FULL_NAME + ", " +
+            commonResources.USERS_COLUMN_FULL_NAME + ", " +
 
-        // Genders.name as gender
-        commonResources.GENDERS_TABLE_NAME + "."
-        + commonResources.GENDERS_COLUMN_NAME + " as " +
-        commonResources.COLUMN_ALIAS_GENDER + ", " +
+            // Genders.name as gender
+            commonResources.GENDERS_TABLE_NAME + "."
+            + commonResources.GENDERS_COLUMN_NAME + " as " +
+            commonResources.COLUMN_ALIAS_GENDER + ", " +
 
-        commonResources.USERS_COLUMN_EXPECTED_SALARY_MIL_VND + ", " +
+            commonResources.USERS_COLUMN_DOB_MILLIS + ", " +
+            commonResources.USERS_COLUMN_AVATAR_URL + " " +
 
-        // TypesOfWork.name as typeOfWork
-        commonResources.TYPES_OF_WORK_TABLE_NAME + "." +
-        commonResources.TYPES_OF_WORK_COLUMN_NAME + " as " +
-        commonResources.COLUMN_ALIAS_TYPE_OF_WORK + ", " +
-
-        commonResources.LEVELS_OF_EDUCATION_COLUMN_NAME + " as " +
-        commonResources.COLUMN_ALIAS_GRADUATED_EDUCATION_LEVEL + ", " +
-
-        commonResources.USERS_COLUMN_YEARS_OF_EXPERIENCE + ", " +
-        commonResources.USERS_COLUMN_RESUME_SUMMARY + ", " +
-        commonResources.USERS_COLUMN_DOB_MILLIS + ", " +
-        commonResources.USERS_COLUMN_ADDRESS + ", " +
-        commonResources.USERS_COLUMN_PHONE + ", " +
-        commonResources.USERS_COLUMN_EMAIL + ", " +
-        commonResources.USERS_COLUMN_AVATAR_URL + ", " +
-        commonResources.USERS_COLUMN_CAREER_OBJECTIVE + " " +
         "from " +
-        commonResources.USERS_TABLE_NAME + " " +
-        "left join " +
-        commonResources.TYPES_OF_WORK_TABLE_NAME + " on " +
-        commonResources.TYPES_OF_WORK_TABLE_NAME + "." +
-        commonResources.TYPES_OF_WORK_COLUMN_ID + " = " +
-        commonResources.USERS_COLUMN_TYPE_OF_WORK_ID + " " +
-        "left join " +
-        commonResources.LEVELS_OF_EDUCATION_TABLE_NAME + " on " +
-        commonResources.LEVELS_OF_EDUCATION_TABLE_NAME + "." +
-        commonResources.LEVELS_OF_EDUCATION_COLUMN_ID + " = " +
-        commonResources.USERS_COLUMN_GRADUATED_EDUCATION_ID + " " +
-        "left join " +
-        commonResources.GENDERS_TABLE_NAME + " on " +
-        commonResources.GENDERS_TABLE_NAME + "." +
-        commonResources.GENDERS_COLUMN_ID + " = " +
-        commonResources.USERS_COLUMN_GENDER_ID + " " +
+            commonResources.USERS_TABLE_NAME + " " +
+            "left join " +
+            commonResources.GENDERS_TABLE_NAME + " on " +
+            commonResources.GENDERS_TABLE_NAME + "." +
+            commonResources.GENDERS_COLUMN_ID + " = " +
+            commonResources.USERS_COLUMN_GENDER_ID + " " +
         "order by " +
         commonResources.USERS_TABLE_NAME + "." +
         commonResources.USERS_COLUMN_ID + " desc";
-
 
     dbConnect.query(
         selectAllUsersSql,
@@ -1981,6 +1955,5 @@ router.post('/education', (req, res) => {
         }
     );
 });
-
 
 module.exports = router;
