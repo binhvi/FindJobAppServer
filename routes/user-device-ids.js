6f995cf266bd733c878374c4eba6cb1dd21cbd75
binhvi <binhvttph07052@fpt.edu.vn>
2021-05-02 15:54:17 +0700

    Delete device ID function on the web

--- routes/user-device-ids.js
+++ routes/user-device-ids.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const commonResources = require('../public/javascripts/common');
 const dbConnect = require('../public/javascripts/db');
+const userDeviceIdsModule = require('./user-device-ids');
 
 router.get('/', (req, res) => {
     // If not logged in, go to log in page
@@ -25,6 +26,36 @@ router.get('/', (req, res) => {
 
             let userDeviceIds = result;
             res.render("user-device-ids/index", {userDeviceIds});
+        }
+    );
+});
+
+router.post('/remove', async (req, res) => {
+    if (req.body.deviceIdString === undefined) {
+        res.send("Thiếu trường deviceIdString");
+        return;
+    }
+
+    let deviceIdString = req.body.deviceIdString.trim();
+    let removeDeviceIdSql =
+        "delete from " +
+        commonResources.USER_DEVICE_IDS_TABLE_NAME + " " +
+        "where " +
+        commonResources.USER_DEVICE_IDS_COL_DEVICE_ID_STRING
+        + " = ?;";
+    dbConnect.query(
+        removeDeviceIdSql,
+        [deviceIdString],
+        function (err, result) {
+            if (err) {
+                // Print err to console to show the admin what's wrong
+                console.log("Có lỗi xảy ra khi xóa mã thiết bị.");
+                console.log(err);
+                res.send("Có lỗi xảy ra khi xóa mã thiết bị");
+                return;
+            }
+
+            res.redirect('/user-device-ids');
         }
     );
 });
